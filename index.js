e87dcf19bc2782af5fa4f67f658bf534502d9f7e
Martin Cameron <mcameron.brix@gmail.com>
2023-11-06 08:52:46 +0000

    applied separation of concerns

--- index.js
+++ index.js
@@ -1,49 +1,6 @@
 import { searchForMatch } from "./searchForMatch.js";
-  
-let puzzle = 
-  [
-    'c', 'i', 's', 'l', 'e', 'f', 'o', 'l', 'i', 'a', 't', 'i', 'o', 'n', 's', 'd',
-    'p', 'y', 'h', 'k', 'z', 't', 'm', 'g', 'h', 'o', 'i', 'o', 'b', 'n', 't', 'z',
-    'e', 'j', 'u', 'b', 'n', 'a', 'i', 'r', 'b', 'm', 'a', 'c', 'o', 'p', 'y', 'f',
-    'n', 's', 't', 'u', 'f', 'g', 'w', 'l', 'd', 'g', 'a', 'r', 'w', 't', 'o', 'l',
-    'o', 'h', 'c', 'i', 't', 'd', 's', 'w', 'o', 'f', 'r', 'g', 'y', 'a', 'l', 'e',
-    't', 'w', 'c', 'h', 'v', 'e', 'k', 'y', 'd', 'b', 't', 'h', 'm', 'c', 'e', 'r',
-    's', 'k', 'r', 'r', 'i', 'g', 'k', 'r', 'm', 'v', 'i', 'f', 'i', 'a', 'w', 'c',
-    'd', 'l', 'u', 'e', 'd', 's', 'b', 'a', 'r', 'u', 'u', 'h', 'r', 'u', 'i', 'i',
-    'n', 'r', 'r', 'm', 'n', 'b', 't', 't', 'u', 'i', 'p', 'g', 'p', 'g', 's', 'h',
-    'a', 't', 'a', 'd', 's', 'i', 'h', 'n', 'j', 'r', 'z', 'y', 'r', 'm', 'i', 'p',
-    's', 'k', 'b', 'p', 'c', 't', 'o', 'e', 'o', 'r', 't', 'u', 'e', 's', 'a', 'r',
-    'g', 'n', 'e', 'i', 's', 's', 't', 'm', 'o', 'o', 'r', 'r', 't', 'e', 'n', 'o', 
-    't', 'j', 'n', 'u', 'c', 'd', 'a', 'i', 'r', 'v', 'a', 'l', 'r', 'u', 'm', 'm',
-    'p', 'e', 'r', 'w', 'b', 't', 'l', 'd', 'e', 'f', 'u', 'h', 'a', 'h', 'r', 'a',
-    'r', 'h', 't', 'b', 'e', 'b', 'q', 'e', 'y', 'a', 'q', 'g', 'c', 'c', 'r', 't',
-    't', 'q', 'w', 'm', 'e', 't', 'r', 's', 'f', 'y', 'j', 'v', 'd', 'r', 'i', 'e',
-    'k', 'g', 'j', 'e', 'n', 'n', 'a', 'e', 't', 'i', 't', 'a', 'm', 'g', 'i', 'm',
-  ];
-
-
-let words = [
-  'amphibolite', 
-  'cambrian', 
-  'dyke', 
-  'faults', 
-  'feldspar', 
-  'felsic', 
-  'foliation', 
-  'gneiss', 
-  'lewisian',
-  'mafic',
-  'magma',
-  'metamorphic',
-  'mica',
-  'migmatite',
-  'moine',
-  'quartz',
-  'sandstone',
-  'schist',
-  'sedimentary',
-  'thrust'
-];
+import { words } from './words.js';
+import { puzzle } from "./puzzle.js"; 
 
 
   function search(words, columns, rows){
---
+++ words.js
@@ -0,0 +1,22 @@
+export let words = [
+    'amphibolite', 
+    'cambrian', 
+    'dyke', 
+    'faults', 
+    'feldspar', 
+    'felsic', 
+    'foliation', 
+    'gneiss', 
+    'lewisian',
+    'mafic',
+    'magma',
+    'metamorphic',
+    'mica',
+    'migmatite',
+    'moine',
+    'quartz',
+    'sandstone',
+    'schist',
+    'sedimentary',
+    'thrust'
+    ];
