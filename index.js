81f626b3c5a9883fed3982f9d7aa28df58e81180
Martin Cameron <mcameron.brix@gmail.com>
2023-11-06 05:47:25 +0000

    updated row calculation, added all logic

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 import { searchForMatch } from "./searchForMatch.js";
-
+  
 let puzzle = 
   [
-    'a', 'a', 't', 'p', 'h', 's', 't', 
-    'n', 'n', 'a', 't', 'h', 'c', 'r', 
-    't', 'd', 'c', 'a', 's', 'a', 'w', 
-    'o', 's', 'o', 'h', 'q', 'g', 'r', 
-    'w', 't', 's', 'w', 'o', 'w', 'y', 
-    'f', 'h', 's', 'j', 'l', 'r', 'c'
+    'a', 'a', 't', 'p', 'h', 'o', 'n', 'e', 'a',
+    'y', 'n', 'a', 't', 'h', 'c', 'r', 'i', 'o',
+    't', 't', 'c', 'a', 's', 'a', 'w', 'w', 'f',
+    'o', 's', 'o', 'h', 'q', 'g', 'r', 'e', 'b',
+    'e', 't', 'o', 'w', 'o', 'w', 'y', 'o', 's',
+    'f', 'h', 's', 'j', 'l', 'r', 'c', 'g', 'h',
   ];
-  
-  let words = [
-    'ant', 
-    'to', 
-    'so', 
-    'hat', 
-    'tap', 
-    'towl', 
-    'pat', 
-    'tow', 
-    'pharoh',
-    'phone',
-    'anchor',
-    'tacos'
-  ];
-  
+
+
+let words = [
+  'ants', 
+  'to', 
+  'so', 
+  'hat', 
+  'tap', 
+  'towl', 
+  'pat', 
+  'tow', 
+  'pharoh',
+  'phone',
+  'anchor',
+  'tacos'
+];
+
+
   function search(words, columns, rows){
     for(let i of words){
-      console.log(i)
         letterAlgo(i, columns, rows);
     }
   };
@@ -35,72 +36,71 @@ let puzzle =
   function letterAlgo(word, columns, rows){
     puzzle.forEach((x, index) => {
       if(x === word[0]){
-        console.log(x, index)
         sortIndices(index, word, columns, rows);
+        return;
+      } else {
         return;
       }
     });
   };
   
   function sortIndices(index, word, columns, rows){
-    let indexPosition = index/(columns);
-    let key = index%(rows-1);
-      //console.log(index%(columns), word, key)
-      if(index/(columns+1) - word.length >= 0){
-        let ans = searchForMatch(index, indexPosition, word, 'left', puzzle, key, rows);
+    let columnPosition = index%columns;
+    let rowPosition =  Math.floor(index / columns);
+      if(columnPosition - word.length >= 0){
+        let ans = searchForMatch(index, columnPosition, word, 'left', puzzle, rowPosition, columns, rows);
         if(ans){
           return true;
         }
       }
-      /*if((columns - index) - word.length >= 0){
-        let ans = searchForMatch(index, word, 'right', puzzle, key);
+      if(columnPosition + word.length <= rows){
+        console.log(word)
+        let ans = searchForMatch(index, columnPosition, word, 'right', puzzle, rowPosition, columns, rows);
         if(ans){
           return ans;
         }
       }
-      if((key + 1) - (word.length) >= 0){
-        let ans = searchForMatch(index, word, 'up', puzzle, key);
+      if((rowPosition + 1) - (word.length) >= 0){
+        let ans = searchForMatch(index, columnPosition, word, 'up', puzzle, rowPosition, columns, rows);
         if(ans){
           return ans;
         }
       } 
-      if((key + 1) + word.length >= 0){
-        let ans = searchForMatch(index, word, 'down', puzzle, key);
+      if(((rowPosition + 1) + word.length) <= rows){
+        let ans = searchForMatch(index, columnPosition, word, 'down', puzzle, rowPosition, columns, rows);
         if(ans){
           return ans;
         }
       } 
-
-      if((index + 1) - word.length >= 0 && (key + 1) - word.length >= 0){
-        let ans = searchForMatch(index, word, 'topLeft', puzzle, key);
+      if((columnPosition + 1) - word.length >= 0 && (rowPosition + 1) - word.length >= 0){
+        let ans = searchForMatch(index, columnPosition, word, 'topLeft', puzzle, rowPosition, columns, rows);
         if(ans){
           return ans;
         }
       }
-      
-      if(word.length - (index + 1) >= 0 && (key + 1) - word.length >= 0){
-        let ans = searchForMatch(index, word, 'topRight',  puzzle, key);
+      if(word.length - (columnPosition + 1) >= 0 && (rowPosition + 1) - word.length >= 0){
+        let ans = searchForMatch(index, columnPosition, word, 'topRight',  puzzle, rowPosition, columns, rows);
         if(ans){
           return ans;
         }
       }
 
-      if(word.length - (index + 1) >= 0 && word.length - (key + 1) >= 0){
-        let ans = searchForMatch(index, word, 'bottomRight',  puzzle, key);
+      if(word.length - (columnPosition + 1) >= 0 && word.length - (rowPosition + 1) >= 0){
+        let ans = searchForMatch(index, columnPosition, word, 'bottomRight',  puzzle, rowPosition, columns, rows);
         if(ans){
           return ans;
         }
       }
 
-      if((index) - word.length >= 0 && word.length - (key + 1) >= 0){
-        let ans = searchForMatch(index, word, 'bottomLeft',  puzzle, key);
+      if((columnPosition) - word.length >= 0 && word.length - (rowPosition + 1) >= 0){
+        let ans = searchForMatch(index, columnPosition, word, 'bottomLeft',  puzzle, rowPosition, columns, rows);
         if(ans){
           return ans;
         }
-      }    */  
+      }    
   };
   
-  search(words, 7, 6)
+  search(words, 9, 6)
 
   /*let date = new Date().getMilliseconds();
   for(let i = 0; i < 101; i++){
